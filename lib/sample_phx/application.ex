d2151eb1c5dc99fa77f63f04249d6a128e91fc8e
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2020-07-18 14:55:49 -0400

    Fetch data from database instead of in-memory store

--- lib/sample_phx/application.ex
+++ lib/sample_phx/application.ex
@@ -8,7 +8,7 @@ defmodule SamplePhx.Application do
   def start(_type, _args) do
     children = [
       # Start the Ecto repository
-      # SamplePhx.Repo,
+      SamplePhx.Repo,
       # Start the Telemetry supervisor
       SamplePhxWeb.Telemetry,
       # Start the PubSub system
--- lib/sample_phx_web/models/user.ex
+++ lib/sample_phx_web/models/user.ex
@@ -1,3 +1,15 @@
 defmodule SamplePhx.User do
-  defstruct [:id, :name, :username, :password]
+  use Ecto.Schema
+  import Ecto.Changeset
+  import Ecto.Query, only: [from: 1, from: 2]
+  alias SamplePhx.User
+
+  schema "users" do
+    field :name, :string
+    field :username, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps()
+  end
 end
