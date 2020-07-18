f50d1b618d4c2aa423513d10182b2f63ba23b5b3
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2020-07-18 12:42:39 -0400

    Basic templates for user pages

--- lib/sample_phx_web/controllers/user_controller.ex
+++ lib/sample_phx_web/controllers/user_controller.ex
@@ -1,12 +1,17 @@
 defmodule SamplePhxWeb.UserController do
   use SamplePhxWeb, :controller
 
+  alias SamplePhx.Repo
+  alias SamplePhx.User
+
   @spec index(Plug.Conn.t(), any) :: Plug.Conn.t()
   def index(conn, _params) do
-    render(conn, "index.html")
+    users = Repo.all(User)
+    render(conn, "index.html", users: users)
   end
 
-  def show(conn, _params) do
-    render(conn, "show.html")
+  def show(conn, %{"id" => id}) do
+    user = Repo.get(User, id)
+    render(conn, "show.html", user: user)
   end
 end
--- lib/sample_phx_web/views/user_view.ex
+++ lib/sample_phx_web/views/user_view.ex
@@ -1,3 +1,11 @@
 defmodule SamplePhxWeb.UserView do
   use SamplePhxWeb, :view
+
+  alias SamplePhx.User
+
+  def first_name(%User{name: name}) do
+    name
+    |> String.split(" ")
+    |> Enum.at(0)
+  end
 end
