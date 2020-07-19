808ccd43dc47ba236b47b6c36a3b7f2fe12cd808
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2020-07-19 09:17:36 -0400

    Add registration using comeonin and bcrypt

--- lib/sample_phx_web/models/user.ex
+++ lib/sample_phx_web/models/user.ex
@@ -17,6 +17,23 @@ defmodule SamplePhx.User do
     |> cast(attrs, [:name, :username, :password])
     |> validate_required([:name, :username, :password])
     |> validate_length(:username, min: 1, max: 20)
-    |> validate_length(:password, min: 6)
+  end
+
+  def registration_changeset(model, attrs) do
+    model
+    |> changeset(attrs)
+    |> cast(attrs, [:password], [])
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: password}} ->
+        put_change(changeset, :password_hash, Bcrypt.hash_pwd_salt(password))
+
+      _ ->
+        changeset
+    end
   end
 end
--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule SamplePhx.MixProject do
   def application do
     [
       mod: {SamplePhx.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :comeonin]
     ]
   end
 
@@ -44,7 +44,9 @@ defmodule SamplePhx.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:comeonin, "~> 5.0"},
+      {:bcrypt_elixir, "~> 2.0"}
     ]
   end
 
