f1598e02df5d1c33185646f3dcca01b88e348b5b
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2020-07-26 19:10:02 -0400

    Install mix test watch

--- mix.exs
+++ mix.exs
@@ -45,7 +45,8 @@ defmodule SamplePhx.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:pbkdf2_elixir, "~> 1.0"}
+      {:pbkdf2_elixir, "~> 1.0"},
+      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false}
     ]
   end
 
