9f6fae7f214acaf866a107854bc028fb56f00fc9
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2020-07-26 10:33:05 -0400

    Update page controller test

--- test/sample_phx_web/controllers/page_controller_test.exs
+++ test/sample_phx_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule SamplePhxWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to Sample Phx!"
   end
 end
