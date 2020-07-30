b022b59ccacd39ef594536c6daff713fa86efa52
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2020-07-30 08:38:11 -0400

    Add JS for video player

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,25 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import "../css/app.scss"
+import '../css/app.scss';
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
-// in "webpack.config.js".
+// in 'webpack.config.js'.
 //
 // Import deps with the dep name or local files with a relative path, for example:
 //
-//     import {Socket} from "phoenix"
-//     import socket from "./socket"
+//     import {Socket} from 'phoenix'
+//     import socket from './socket'
 //
-import "phoenix_html"
+import 'phoenix_html';
+
+import Player from './player';
+
+let video = document.getElementById('video');
+
+if (video) {
+  Player.init(video.id, video.getAttribute('data-player-id'), () => {
+    console.log('player ready');
+  });
+}
