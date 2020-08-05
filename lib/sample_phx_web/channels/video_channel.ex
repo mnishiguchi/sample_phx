d250bdf630b7453889c254e7fe9b4b6302720d06
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2020-08-05 19:42:23 -0400

    Ping every 5 seconds

--- lib/sample_phx_web/channels/video_channel.ex
+++ lib/sample_phx_web/channels/video_channel.ex
@@ -8,6 +8,19 @@ defmodule SamplePhxWeb.VideoChannel do
   the rest of the topic to the video_id variable.
   """
   def join("videos:" <> video_id, _params, socket) do
-    {:ok, assign(socket, :video_id, String.to_integer(video_id))}
+    :timer.send_interval(5_000, :ping)
+    {:ok, socket}
+  end
+
+  @doc """
+  Takes the existing count (or a default of 1) and increments that count.
+  Invoked whenever an Elixir message reaches the channel. In this case, we match
+  on the periodic `:ping` message and increase a counter every time it arrives.
+  """
+  def handle_info(:ping, socket) do
+    count = socket.assigns[:count] || 1
+    push(socket, "ping", %{count: count})
+
+    {:noreply, assign(socket, :count, count + 1)}
   end
 end
