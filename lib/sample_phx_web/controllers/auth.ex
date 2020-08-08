f3187cdef06b3413fa19bcfb5613d1c9ef33f656
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2020-08-08 15:12:02 -0400

    Socket authentication

--- lib/sample_phx_web/controllers/auth.ex
+++ lib/sample_phx_web/controllers/auth.ex
@@ -11,14 +11,14 @@ defmodule SamplePhxWeb.Auth do
     user_id = get_session(conn, :user_id)
 
     cond do
-      # If current_user is already present in conn.assigns, we honor it, no matter how it got there.
-      # It makes our code more testable.
-      conn.assigns[:current_user] ->
-        conn
+      # If `current_user` is already present in `conn.assigns`, we honor it no matter how it got
+      # there. It makes our code more testable.
+      user = conn.assigns[:current_user] ->
+        put_current_user(conn, user)
 
+      # Look for a user in the database and put it as `current_user` if found.
       user = user_id && SamplePhx.Accounts.get_user(user_id) ->
-        # Make current_user available in all downstream functions including controllers and views.
-        assign(conn, :current_user, user)
+        put_current_user(conn, user)
 
       true ->
         assign(conn, :current_user, nil)
@@ -39,7 +39,7 @@ defmodule SamplePhxWeb.Auth do
 
   def login(conn, user) do
     conn
-    |> assign(:current_user, user)
+    |> put_current_user(user)
     |> put_session(:user_id, user.id)
     # Tell Plug to send the session cookie back to the client with a different identifier in case
     # an attacker knew by any chance the previous one.
@@ -48,5 +48,17 @@ defmodule SamplePhxWeb.Auth do
 
   def logout(conn) do
     configure_session(conn, drop: true)
+  end
+
+  @doc """
+  Makes `current_user` available in all downstream functions including controllers and views. Also,
+  assigns a logged-in user token for the socket.
+  """
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
   end
 end
